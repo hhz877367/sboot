9363743c3cecf65d02e6c7fbe8630b49d428fc60
hhz_fork_junti <huhongzhi@yoonuu.com>
2022-02-18 22:43:43 +0800

    hhz-2021-18 22;22

---
+++ sboot/src/main/java/com/hhz/model/datastruture/dev/Test1.java
@@ -0,0 +1,7 @@
+package com.hhz.model.datastruture.dev;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("day01");
+    }
+}
---
+++ sboot/src/main/java/com/hhz/model/datastruture/dev/Test3.java
@@ -0,0 +1,7 @@
+package com.hhz.model.datastruture.dev;
+
+public class Test3 {
+    public static void main(String[] args) {
+        System.out.println("day03");
+    }
+}
