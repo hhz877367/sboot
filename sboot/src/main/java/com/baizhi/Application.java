b6ea18068151d3965e33e2334f9d8487099b536e
hhz_fork_junti <huhongzhi@yoonuu.com>
2022-02-18 18:13:21 +0800

    hhz-2021-18

--- sboot/src/main/java/com/baizhi/Application.java
+++ sboot/src/main/java/com/baizhi/Application.java
@@ -13,15 +13,14 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @EnableGlobalMethodSecurity(securedEnabled = true)
 @SpringBootApplication()
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ sboot启动成功   ლ(´ڡ`ლ)ﾞ  \n" + " .-------.       ____     __  " + "   " +
-            "   \n" + " |  _ _   \\      \\   \\   /  /     \n" + " | ( ' )  |       \\  _. " +
-            "/ " + " '       \n" + " |(_ o _) /        _( )_ .'         \n" + " | (_,_).' " +
-            "__ " + " ___(_ " + "o _)'          \n" + " |  |\\ \\  |  ||   |(_,_)'         " +
-            "\n" + " | " + " | \\ `'   " + "/|   `-'  /           \n" +
-            " |  |  \\    /  \\  " + "    /          " + " \n" + " ''-'   " +
-            "`'-'    `-..-'              ");
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+    System.out.println("(♥◠‿◠)ﾉﾞ sboot启动成功   ლ(´ڡ`ლ)ﾞ  \n" + " .-------.       ____     __  " + "   " +
+        "   \n" + " |  _ _   \\      \\   \\   /  /     \n" + " | ( ' )  |       \\  _. " +
+        "/ " + " '       \n" + " |(_ o _) /        _( )_ .'         \n" + " | (_,_).' " +
+        "__ " + " ___(_ " + "o _)'          \n" + " |  |\\ \\  |  ||   |(_,_)'         " +
+        "\n" + " | " + " | \\ `'   " + "/|   `-'  /           \n" +
+        " |  |  \\    /  \\  " + "    /          " + " \n" + " ''-'   " +
+        "`'-'    `-..-'              ");
+  }
 }
