8fcb354023b4f1583ec6a5dd216fa84abcbe511c
hhz_fork_junti <huhongzhi@yoonuu.com>
2022-03-05 12:47:01 +0800

    2022-3-5

--- sboot/src/main/java/com/hhz/model/datastruture/MyQueue.java
+++ sboot/src/main/java/com/hhz/model/datastruture/MyQueue.java
@@ -5,20 +5,73 @@ import java.util.Queue;
 
 public class MyQueue {
     public static void main(String[] args) {
-        Queue<String> list = new LinkedList<>();
-        list.add("aaa");
-        list.add("bbb");
-        list.add("ccc");
+        MyQueue myQueue = new MyQueue(10);
+        myQueue.push(0);
+        myQueue.push(1);
+        myQueue.push(2);
+        myQueue.push(3);
+        myQueue.push(4);
+        myQueue.push(5);
+        myQueue.push(6);
+        myQueue.push(7);
+        myQueue.push(8);
+        myQueue.push(9);
+        myQueue.pop();
+        myQueue.pop();
+        myQueue.pop();
+        myQueue.pop();
+        myQueue.pop();
+        myQueue.pop();
+        myQueue.pop();
+        myQueue.pop(); //8个 ，剩余 89
+        myQueue.print();
 
-        //返回第一个元素，并且在队列里删除
-        System.out.println(list.poll());
-        //返回第一个元素，在队列里不删除
-        System.out.println(list.peek());
-        System.out.println(list.element());
-        System.out.println(list.element());
-
-
-
-        
     }
+
+
+    private int data[];
+    private int head = 0;
+    private int tail = 0;
+
+    private int n = 0; //数组的总共大小
+    private int size=0;
+
+
+    public MyQueue(int n) {
+        data = new int[n];
+        this.n = n;
+    }
+
+    //入队
+    public boolean push(int value) {
+        //如果队列已满
+        if (size==n) {
+            return false;
+        }
+        //寻找tail 的下标
+        data[tail] = value;
+        tail = (tail + 1) % n;
+        size++;
+        return true;
+    }
+
+
+    public int pop() {
+        //查看队列是否为空
+        if (size==0) {
+            return -1;
+        }
+        int m=data[head];
+        head = (head + 1) % n;
+        size--;
+        return m;
+    }
+
+    //遍历
+    public void print(){
+        for(;head!=tail;head=(head+1)%n){
+            System.out.println(data[head]);
+        }
+    }
+
 }
